4b8117b2711221aae2108f4ff0965fcbcacb4b02
Sourenakia <sourena85kia@gmail.com>
2025-01-31 02:39:28 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -8,11 +8,14 @@ typedef struct{
     char name[40];
     char password[40];
     char email[40];
+    
 }user;
 
 typedef struct{
 	int color;
-	char font;
+	char font, location;
+    int flag;
+    int x, y;
 }mainpixel;
 mainpixel pixel[38][153];
 
@@ -37,25 +40,211 @@ void update_screen(){
 	}
 }
 
+void Masir(int yv1, int xv1, int yv2, int xv2){
+    
+    int x1, x2, y1, y2;
+
+
+
+    if(pixel[yv1][xv1 + 1].flag == 0){
+        x1 = xv1 + 1;
+        y1 = yv1;
+    }
+    else if(pixel[yv1][xv1 - 1].flag == 0){
+        x1 = xv1 - 1;
+        y1 = yv1;
+    }
+    else if(pixel[yv1 - 1][xv1].flag == 0){
+        x1 = xv1;
+        y1 = yv1 - 1;
+    }
+    else if(pixel[yv1 + 1][xv1].flag == 0){
+        x1 = xv1;
+        y1 = yv1 + 1;
+    }
+    if(pixel[yv2][xv2 + 1].flag == 0){
+        x2 = xv2 + 1;
+        y2 = yv2;
+    }
+    else if(pixel[yv2][xv2 - 1].flag == 0){
+        x2 = xv2 - 1;
+        y2 = yv2;
+    }
+    else if(pixel[yv2 - 1][xv2].flag == 0){
+        x2 = xv2;
+        y2 = yv2 - 1;
+    }
+    else if(pixel[yv2 + 1][xv2].flag == 0){
+        x2 = xv2;
+        y2 = yv2 + 1;
+    }
+    if(x2 < x1){
+        int temp = x1;
+        x1 = x2;
+        x2 = x1;
+    }
+    
+    if(y1 > y2){
+        for(int i = x1; i <= x2; i++){
+            if(pixel[y2][i].flag == 0){
+                pixel[y2][i].font = '#';
+            }
+        }
+        for(int j = y1; j >= y2; j--){
+            if(pixel[j][x1].flag == 0){
+                pixel[j][x1].font = '#';
+            }
+        }    
+    }
+    else{
+        for(int i = x1; i <= x2; i++){
+            if(pixel[y1][i].flag == 0){
+                pixel[y1][i].font = '#';
+            }
+        }
+        for(int j = y2; j >= y1; j--){
+            if(pixel[j][x2].flag == 0){
+                pixel[j][x2].font = '#';
+            }
+        }
+    }
+    
+    //check
+}
+
+
+
+
+
+
+
+
+void Masir_1(int yv1, int xv1, int yv2, int xv2){
+    
+    int x1, x2, y1, y2;
+
+
+
+    if(pixel[yv1][xv1 + 1].flag == 0){
+        x1 = xv1 + 1;
+        y1 = yv1;
+    }
+    else if(pixel[yv1][xv1 - 1].flag == 0){
+        x1 = xv1 - 1;
+        y1 = yv1;
+    }
+    else if(pixel[yv1 - 1][xv1].flag == 0){
+        x1 = xv1;
+        y1 = yv1 - 1;
+    }
+    else if(pixel[yv1 + 1][xv1].flag == 0){
+        x1 = xv1;
+        y1 = yv1 + 1;
+    }
+    if(pixel[yv2][xv2 + 1].flag == 0){
+        x2 = xv2 + 1;
+        y2 = yv2;
+    }
+    else if(pixel[yv2][xv2 - 1].flag == 0){
+        x2 = xv2 - 1;
+        y2 = yv2;
+    }
+    else if(pixel[yv2 - 1][xv2].flag == 0){
+        x2 = xv2;
+        y2 = yv2 - 1;
+    }
+    else if(pixel[yv2 + 1][xv2].flag == 0){
+        x2 = xv2;
+        y2 = yv2 + 1;
+    }
+    
+    if(x1 > x2){
+        for(int i = x1; i >= x2; i--){
+            if (pixel[y1][i].flag == 0){
+                pixel[y1][i].font = '#';
+            }
+        }
+        for(int j = y1; j <= y2; j++){
+            if(pixel[j][x2].flag == 0){
+                pixel[j][x2].font = '#';
+            }
+        }
+    }
+    else{
+        for(int j = y2; j >= y1; j--){
+            if(pixel[j][x1].flag == 0){
+                pixel[j][x1].font = '#';
+            }
+        }
+        for(int i = x1; i <= x2; i++){
+            if(pixel[y2][i].flag == 0){
+                pixel[y2][i].font = '#';
+            }
+        }
+    }
+    
+    //check
+}
+
 int jadval(int ax, int bx, int ay, int by){
 	for(int i = ax; i <= bx; i++){
 		pixel[ay][i].font = '-';
 		pixel[by][i].font = '-';
+        pixel[ay][i].flag = 2;
+		pixel[by][i].flag = 2;
 	}
 	for(int i = ay + 1; i < by; i++){
 		pixel[i][ax].font = '|';
 		pixel[i][bx].font = '|';
+        pixel[i][ax].flag = 3;
+        pixel[i][bx].flag = 3;
 	}
 	for(int i = ax + 1; i < bx; i++){
 		for(int j = ay + 1; j < by; j++){
 			pixel[j][i].font = '.';
+            pixel[j][i].flag = 1;
 		}
 	}
-	update_screen();
+    int p = 0, p1 = 0, p2 = 0, p3 = 0, p4 = 0;
+    do{
+        int px1 = Random_number(ax + 1, bx - 1);
+        int px2 = Random_number(ax + 1, bx - 1);
+        int py1 = Random_number(ay + 1, by - 1);
+        int py2 = Random_number(ay + 1, by - 1);
+        int random = rand() % 2;
+        if((random == 1) && (p1 == 0)){
+            p1 = 1;
+            pixel[ay][px1].font = '+';
+            pixel[ay][px1].flag = 5;
+            pixel[ay][px1].location = 'U';
+        }
+        random = rand() % 2;
+        if((random == 1) && (p2 == 0)){
+            p2 = 1;
+            pixel[by][px2].font = '+';
+            pixel[by][px2].flag = 5;
+            pixel[by][px2].location = 'D';
+        }
+        random = rand() % 2;
+        if((random == 1) && (p3 == 0)){
+            p3 = 1;
+            pixel[py1][ax].font = '+';
+            pixel[py1][ax].flag = 5;
+            pixel[py1][ax].location = 'L';
+        }
+        random = rand() % 2;
+        if((random == 1) && (p4 == 0)){
+            p4 = 1;
+            pixel[py2][bx].font = '+';
+            pixel[py2][bx].flag = 5;
+            pixel[py2][bx].location = 'R';
+        }
+        p = p1 + p2 + p3 + p4;
+    } while (p != 3 && p != 4);
 }
 
 void Table_show(int x1, int x2, int y1, int y2){
-    int randomy1, randomx1, randomy2, randomx2;
+    int randomy1, randomx1, randomy2, randomx2, p = 0;
     do{
         randomy1 = Random_number(y1, y2);
         randomy2 = Random_number(y1, y2);
@@ -80,71 +269,239 @@ void Table_show(int x1, int x2, int y1, int y2){
     }while (true);
 }
 
+int Rahroh_up(int a, int b, int c, int d, int e){
+    int m1, n1, m2, n2, p1 = 0, p2 = 0;
+    for(int j = a; j < b; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'U'){
+                m1 = j;
+                n1 = i;
+                p1++;
+            }
+        }
+    }
+    for(int j = a; j < b; j++){
+        for(int i = d + 1; i < e; i++){
+            if(pixel[j][i].location == 'L'){
+                m2 = j;
+                n2 = i;
+                p2++;
+            }
+        }
+    }
+    if (p1 + p2 == 2){
+        Masir(m1, n1, m2, n2);
+    }
+    else if(p1 == 1){
+        for(int j = a; j < b; j++){
+            for(int i = d + 1; i < e; i++){
+                if(pixel[j][i].location == 'U'){
+                    m2 = j;
+                    n2 = i;
+                    p2++;
+                }
+            }
+        }
+        Masir(m1, n1, m2, n2);
+    }
+}
 
+int Rahoroh_right(int a, int b, int c, int d, int e){
+    int m1, n1, m2, n2, p1 = 0, p2 = 0;
+    for(int j = a; j < b; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'R'){
+                m1 = j;
+                n1 = i;
+                p1++;
+            }
+        }
+    }
+    for(int j = a; j < b; j++){
+        for(int i = d + 1; i < e; i++){
+            if(pixel[j][i].location == 'L'){
+                m2 = j;
+                n2 = i;
+                p2++;
+            }
+        }
+    }
+    if (p1 + p2 == 2){
+        Masir(m1, n1, m2, n2);
+    }
+    else if(p1 == 1){
+        for(int j = a; j < b; j++){
+            for(int i = d + 1; i < e; i++){
+                if(pixel[j][i].location == 'U'){
+                    m2 = j;
+                    n2 = i;
+                    p2++;
+                }
+            }
+        }
+        Masir(m1, n1, m2, n2);
+    }
+    else{
+        Rahroh_up(a, b, c, d, e);
+    }
+}
+
+int Rahroh_left(int a, int b, int c, int d, int e){
+    int m1, n1, m2, n2, p1 = 0, p2 = 0;
+    for(int j = a; j < b; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'L'){
+                m1 = j;
+                n1 = i;
+                p1++;
+            }
+        }
+    }
+    for(int j = b + 1; j < e; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'U'){
+                m2 = j;
+                n2 = i;
+                p2++;
+        }
+        }
+    }
+    if (p1 + p2 == 2){
+        Masir_1(m1, n1, m2, n2);
+    }
+    else if(p1 == 1){
+        for(int j = b + 1; j < e; j++){
+            for(int i = c; i < d; i++){
+                if(pixel[j][i].location == 'L'){
+                    m2 = j;
+                    n2 = i;
+                    p2++;
+                }
+            }
+        }
+        Masir_1(m1, n1, m2, n2);
+    }
+}
+
+int Rahroh_down(int a, int b, int c, int d, int e){
+    int m1, n1, m2, n2, p1 = 0, p2 = 0;
+    for(int j = a; j < b; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'D'){
+                m1 = j;
+                n1 = i;
+                p1++;
+            }
+        }
+    }
+    for(int j = b + 1; j < e; j++){
+        for(int i = c; i < d; i++){
+            if(pixel[j][i].location == 'U'){
+                m2 = j;
+                n2 = i;
+                p2++;
+        }
+        }
+    }
+    if (p1 + p2 == 2){
+        Masir_1(m1, n1, m2, n2);
+    }
+    else if(p1 == 1){
+        for(int j = b + 1; j < e; j++){
+            for(int i = c; i < d; i++){
+                if(pixel[j][i].location == 'L'){
+                    m2 = j;
+                    n2 = i;
+                    p2++;
+                }
+            }
+        }
+        Masir_1(m1, n1, m2, n2);
+    }
+    else{
+        Rahroh_left(a, b, c, d, e);
+    }
+}
 
 int Main_game(){
     srand(time(0));
-    int tedad = Random_number(6, 10);
-    if(tedad == 6){
-        Table_show(2, 49, 2, 16);
-        Table_show(51, 100, 2, 16);
-        Table_show(102, 150, 2, 16);
-        Table_show(2, 49, 18, 35);
-        Table_show(51, 100, 18, 35);
-        Table_show(102, 150, 18, 35);
+    int tedad = Random_number(11, 15);
+    tedad = 12;
+    /*/
+    if(tedad == 11){
+        Table_show(4, 36, 4, 12);
+        Table_show(38, 74, 4, 12);
+        Table_show(76, 112, 4, 12);
+        Table_show(114, 148, 4, 12);
+        Table_show(4, 49, 14, 23);
+        Table_show(51, 100, 14, 23);
+        Table_show(102, 148, 14, 23);
+        Table_show(4, 36, 25, 32);
+        Table_show(38, 74, 25, 32);
+        Table_show(76, 112, 25, 32);
+        Table_show(114, 148, 25, 32);
+        int m1, m2, n1, n2;
+        for(int j = 4; j < 13; j++){
+            for(int i = 4; i < 37; i++){
+                if(pixel[j][i].flag == 5){
+                    m1 = i;
+                    n1 = j;
+                }
+            }
+        }
+        for(int j = 4; j < 13; j++){
+            for(int i = 38; i < 75; i++){
+                if(pixel[j][i].flag == 5){
+                    m2 = i;
+                    n2 = j;
+                }
+            }
+        }
+        Masir(n1, m1, n2, m2);
+        //mvprintw(10, 50, "%d %d %d %d", n1, n2, m1, m2);
     }
-    else if(tedad == 7){
-        Table_show(2, 49, 2, 16);
-        Table_show(51, 100, 2, 16);
-        Table_show(102, 150, 2, 16);
-        Table_show(2, 36, 18, 35);
-        Table_show(38, 74, 18, 35);
-        Table_show(76, 112, 18, 35);
-        Table_show(114, 150, 18, 35);
+    */
+    if(tedad == 12){
+        Table_show(4, 36, 4, 12);
+        Table_show(38, 74, 4, 12);
+        Table_show(76, 112, 4, 12);
+        Table_show(114, 148, 4, 12);
+        Table_show(4, 36, 14, 23);
+        Table_show(38, 74, 14, 23);
+        Table_show(76, 112, 14, 23);
+        Table_show(114, 148, 14, 23);
+        Table_show(4, 36, 25, 32);
+        Table_show(38, 74, 25, 32);
+        Table_show(76, 112, 25, 32);
+        Table_show(114, 148, 25, 32);
+        Rahoroh_right(4, 13, 4, 37, 75);
+        Rahoroh_right(4, 13, 38, 75, 113);
+        Rahoroh_right(4, 13, 76, 113, 148);
+        Rahoroh_right(14, 24, 4, 37, 75);
+        Rahoroh_right(14, 24, 38, 75, 113);
+        Rahoroh_right(14, 24, 76, 113, 148);
+        Rahoroh_right(25, 33, 4, 37, 75);
+        Rahoroh_right(25, 33, 38, 75, 113);
+        Rahoroh_right(25, 33, 76, 113, 148);
+        Rahroh_down(4, 13, 4, 37, 24);
+        Rahroh_down(14, 24, 4, 37, 33);
+        Rahroh_down(4, 13, 38, 75, 24);
+        Rahroh_down(14, 24, 38, 75, 33);
+        Rahroh_down(4, 13, 76, 113, 24);
+        Rahroh_down(14, 24, 76, 113, 33);
+        Rahroh_down(4, 13, 114, 149, 24);
+        Rahroh_down(14, 24, 114, 149, 33);
     }
-    else if(tedad == 8){
-        Table_show(2, 36, 2, 16);
-        Table_show(38, 74, 2, 16);
-        Table_show(76, 112, 2, 16);
-        Table_show(114, 150, 2, 16);
-        Table_show(2, 36, 18, 35);
-        Table_show(38, 74, 18, 35);
-        Table_show(76, 112, 18, 35);
-        Table_show(114, 150, 18, 35);
-    }
-    else if(tedad == 9){
-        Table_show(2, 28, 2, 16);
-        Table_show(30, 58, 2, 16);
-        Table_show(60, 88, 2, 16);
-        Table_show(90, 118, 2, 16);
-        Table_show(120, 150, 2, 16);
-        Table_show(2, 36, 18, 35);
-        Table_show(38, 74, 18, 35);
-        Table_show(76, 112, 18, 35);
-        Table_show(114, 150, 18, 35);
-    }
-    else if(tedad == 10){
-        Table_show(2, 28, 2, 16);
-        Table_show(30, 58, 2, 16);
-        Table_show(60, 88, 2, 16);
-        Table_show(90, 118, 2, 16);
-        Table_show(120, 150, 2, 16);
-        Table_show(2, 28, 18, 35);
-        Table_show(30, 58, 18, 35);
-        Table_show(60, 88, 18, 35);
-        Table_show(90, 118, 18, 35);
-        Table_show(120, 150, 18, 35);
-    }
+    update_screen();
     getch();
     Login();
+    
 }
 
 
 
-
 int Check_password(){
-    
-    /*/
+    /*
     clear();
     FILE *fptr;
     user userinfo, temp; 
@@ -162,10 +519,30 @@ int Check_password(){
     attron(COLOR_PAIR(3));
     mvprintw(32, 54, "Press R to return to the previous page");
     attroff(COLOR_PAIR(3));
+    int p;
     do{
-        mvscanw(11, 50, "%s", userinfo.name);
-
-    }while();
+        p = 1;
+        fptr = (fopen("useesinfo.dat","rb+"));
+        mvprintw(11, 50, "                                                                  ");
+        attron(COLOR_PAIR(3));
+        char string[40];
+        mvscanw(11,50, "%s", string);
+        fread(&temp, sizeof(user), 1, fptr);
+        while(!feof(fptr) && p){
+            if(!strcmp(string, userinfo.name)){
+                attron(COLOR_PAIR(1));
+                mvprintw(13, 50, "please enter your password:");
+                attroff(COLOR_PAIR(1));
+                mvscanw(14, 50, "%s", string);
+                if(!(string, userinfo.password)){
+                    mvprintw(31, 54, "Wellcome %s!", userinfo.name);
+                    mvprintw(32, 54, "Press any key to get back to the previous page");
+                    break;
+                }
+                break;
+            } 
+        }
+    }while(!p);
     mvprintw(32, 54, "                                                                  ");
     attron(COLOR_PAIR(4));
     mvprintw(32, 54, "Hello %s!!!", userinfo.name);
@@ -241,7 +618,7 @@ int Check_password(){
         mvprintw(32, 50, "                                                                                      ");
     }while(true);
     return ;
-    */
+    /*/
 
 }
 
@@ -484,6 +861,11 @@ int main(){
     fptr = (fopen("useesinfo.dat","rb+"));
     if (fptr == NULL){
         fptr = (fopen("useesinfo.dat","wb"));
+    }
+    for(int j = 0; j < 38; j++){
+        for(int i = 0; i < 153; i++){
+            pixel[j][i].flag = 0;
+        }
     }
     while(true){
         initscr();
